3c7cba0bdb8e91c1842de633b645c5032bb11b54
Ryuichi Sakagami <shagamiiiii@gmail.com>
2016-08-31 14:11:54 +0900

    add code

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(function(){
       // 移動先を取得
       var target = $(href == "#" || href == "" ? 'html' : href);
       // 移動先を数値で取得
-      var position = target.offset().top - 200;
+      var position = target.offset().top - 210;
       // スムーススクロール
       $('body,html').animate({scrollTop:position}, speed, 'swing');
       return false;
